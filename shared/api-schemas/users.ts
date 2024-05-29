06fa06c00bdc451d7da704279b2de213f1f7ce9b
Miodec <jack@monkeytype.com>
2024-05-29 10:54:02 +0200

    change folder structure

---
+++ shared/api-schemas/users.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const CreateNewUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  captcha: z.string(),
+});
+
+export type CreateNewUserBodyType = z.infer<typeof CreateNewUserBodySchema>;
+
+export type CreateNewUserResponseType = null;
